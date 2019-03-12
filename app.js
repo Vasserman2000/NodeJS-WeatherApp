5d67da164405bfd2394c9f7fe0fd07a6ed5e2d6a
Vasserman2000 <vasserman2000@gmail.com>
2019-03-12 22:56:34 +0200

    starting exploring the darksky api

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-console.log('Starting')
+const request = require('request');
 
+const url = 'https://api.darksky.net/forecast/2a82c8bdfe73c0c8788432b45fb9ee99/37.8267,-122.4233?units=si&lang=ru';
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
-
-
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url, json: true }, (error, response) => {
+    const currently = response.body.currently;
+    console.log(response.body.daily.data[0].summary + ' It is currently ', currently.temperature, ' degrees out. There is a', currently.precipProbability, '% chanse of rain.');
+})
