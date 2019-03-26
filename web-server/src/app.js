a318e7a90d661e2ffa171fc57e84274934696abb
Vasserman2000 <vasserman2000@gmail.com>
2019-03-26 08:38:57 +0200

    added html files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
 
 // if someone requests this page, this function would describe what will be sent back
 app.get('', (req, res) => {
     res.send('<h1 style="background:yellow">Hello express!</h1>')
 });
 
-app.get('/help', (req, res) => {
-    res.send('Help page');
-});
-
-app.get('/about', (req, res) => {
-    res.send({
-        name: 'Elisha',
-        age: 33
-    });
-});
 
 app.get('/weather', (req, res) => {
     res.send('Your weather');
