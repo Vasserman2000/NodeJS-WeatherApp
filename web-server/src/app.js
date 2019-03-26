1551f788e9dcebef7beefe728cd25a04bce35cd9
Vasserman2000 <vasserman2000@gmail.com>
2019-03-26 08:52:38 +0200

    ''

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,12 +7,6 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-// if someone requests this page, this function would describe what will be sent back
-app.get('', (req, res) => {
-    res.send('<h1 style="background:yellow">Hello express!</h1>')
-});
-
-
 app.get('/weather', (req, res) => {
     res.send('Your weather');
 });
