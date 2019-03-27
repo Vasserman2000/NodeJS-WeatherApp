86d064f6a78465118be3f0fc38ff6d8de7d550d0
Vasserman2000 <vasserman2000@gmail.com>
2019-03-27 17:24:47 +0200

    'hbs'

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,9 +3,38 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Define pathsfor Express cinfig
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'ElishaV'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        author: 'Elishav',
+        year: '2019'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        author: 'Elishav',
+        year: '2019'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send('Your weather');
@@ -13,5 +42,5 @@ app.get('/weather', (req, res) => {
 
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000...');
+    console.log('Server is up on port 3000...'); 
 });
